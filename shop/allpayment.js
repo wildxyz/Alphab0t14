ad68eecf5ddebae3cd0b2c1a4ec1cb7fd9a1da68
wildxyz <97534596+wildxyz@users.noreply.github.com>
2022-01-11 15:26:47 +0000

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,12 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+return `PAYMENT BY iWild
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0815 1124 5024
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama M****
 
 *📲 PULSA*
 
