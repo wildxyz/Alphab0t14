3a701849266a971d57dcc93c2e0ff423715e8560
wildxyz <97534596+wildxyz@users.noreply.github.com>
2022-01-11 15:31:11 +0000

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,12 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
+return `DONASI BISA VIA E-MONEY
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0815 1124 5024
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama R*******
 `
 	}
 
